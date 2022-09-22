3645f5a82cba39c2f7a0bd8956a861661d0299a3
Theo-Rokas <T.Rokas@hotmail.com>
2022-09-22 11:57:23 +0300

    First Commit

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 import axios from "axios"
 
@@ -11,6 +11,8 @@ export default function Register() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState(false)
+
+    const navigate = useNavigate()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -24,7 +26,7 @@ export default function Register() {
                 password
             })
 
-            res.data && window.location.replace("/login")
+            res.data && navigate("/login")
         }
         catch(err) {
             setError(true)
